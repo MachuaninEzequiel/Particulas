29c5ad419471021f9d7e0b78d552a2bfe179ea03
MachuaninEzequiel <ezequieladrianmachuanin@gmail.com>
2024-10-30 14:14:43 -0300

    Cambio de colores y tamaños de las particulas

--- simuladorParticulas.py
+++ simuladorParticulas.py
@@ -19,10 +19,13 @@ for _ in range(NUM_PARTICLES):
     y = random.randint(box_rect.top + 10, box_rect.bottom - 10)
     vx = random.uniform(-1, 1)
     vy = random.uniform(-1, 1)
-    particles.append({"pos": [x, y], "vel": [vx, vy]})
+    size = random.randint(5, 15)
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    particles.append({"pos": [x, y], "vel": [vx, vy], "size": size, "color": color})
 
 running = True
 clock = pygame.time.Clock()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,6 +41,16 @@ while running:
     if keys[pygame.K_DOWN]:
         box_rect.y += box_speed
 
+    if keys[pygame.K_g]:  
+        gravity += 0.01
+    if keys[pygame.K_h]:  
+        gravity -= 0.01
+
+    if keys[pygame.K_PLUS] or keys[pygame.K_KP_PLUS]:  
+        box_speed += 1
+    if keys[pygame.K_MINUS] or keys[pygame.K_KP_MINUS]:  
+        box_speed = max(1, box_speed - 1)
+
     screen.fill((0, 0, 0))
     
     pygame.draw.rect(screen, (255, 0, 0), box_rect, 2)
@@ -47,30 +60,27 @@ while running:
         particle["pos"][0] += particle["vel"][0]
         particle["pos"][1] += particle["vel"][1]
 
-        if particle["pos"][0] < box_rect.left + 5 or particle["pos"][0] > box_rect.right - 5:
+        if particle["pos"][0] < box_rect.left + particle["size"] or particle["pos"][0] > box_rect.right - particle["size"]:
             particle["vel"][0] *= -1
-            if particle["pos"][0] < box_rect.left + 5:
-                particle["pos"][0] = box_rect.left + 5
+            if particle["pos"][0] < box_rect.left + particle["size"]:
+                particle["pos"][0] = box_rect.left + particle["size"]
             else:
-                particle["pos"][0] = box_rect.right - 5
+                particle["pos"][0] = box_rect.right - particle["size"]
 
-        if particle["pos"][1] < box_rect.top + 5 or particle["pos"][1] > box_rect.bottom - 5:
+        if particle["pos"][1] < box_rect.top + particle["size"] or particle["pos"][1] > box_rect.bottom - particle["size"]:
             particle["vel"][1] *= -1
-            if particle["pos"][1] < box_rect.top + 5:
-                particle["pos"][1] = box_rect.top + 5
+            if particle["pos"][1] < box_rect.top + particle["size"]:
+                particle["pos"][1] = box_rect.top + particle["size"]
             else:
-                particle["pos"][1] = box_rect.bottom - 5
+                particle["pos"][1] = box_rect.bottom - particle["size"]
 
-        if (particle["pos"][0] < box_rect.left or 
-            particle["pos"][0] > box_rect.right or 
-            particle["pos"][1] < box_rect.top or 
-            particle["pos"][1] > box_rect.bottom):
-            if particle["pos"][0] < box_rect.left or particle["pos"][0] > box_rect.right:
-                particle["vel"][0] *= -1
-            if particle["pos"][1] < box_rect.top or particle["pos"][1] > box_rect.bottom:
-                particle["vel"][1] *= -1
+        pygame.draw.circle(screen, particle["color"], (int(particle["pos"][0]), int(particle["pos"][1])), particle["size"])
 
-        pygame.draw.circle(screen, (255, 255, 255), (int(particle["pos"][0]), int(particle["pos"][1])), 5)
+    # Mostrar estadísticas en pantalla
+    font = pygame.font.SysFont(None, 36)
+    stats_text = f"Gravedad: {gravity:.2f} | Velocidad de Caja: {box_speed}"
+    text_surface = font.render(stats_text, True, (255, 255, 255))
+    screen.blit(text_surface, (10, 10))
 
     pygame.display.flip()
     clock.tick(60)
